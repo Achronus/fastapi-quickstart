95e8d13bc0e459fc843e7a4d25fc36cae547fadd
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-01-05 08:23:22 +0000

    updated project paths

--- fastapi_quickstart/setup/clean.py
+++ fastapi_quickstart/setup/clean.py
@@ -3,7 +3,7 @@ import shutil
 import subprocess
 
 from ..conf.helper import tw_executable_exists
-from ..conf.constants.filepaths import ProjectDirPaths
+from ..conf.constants.filepaths import ProjectPaths
 from .base import ControllerBase
 
 
@@ -23,8 +23,8 @@ class CleanupController(ControllerBase):
     def node_modules() -> None:
         """Removes the `node_modules` folder if `tailwindcss` does not exist."""
         # If exists, remove node_modules
-        if tw_executable_exists(ProjectDirPaths.PROJECT):
-            shutil.rmtree(os.path.join(ProjectDirPaths.PROJECT, 'node_modules'))
+        if tw_executable_exists(ProjectPaths.PROJECT):
+            shutil.rmtree(os.path.join(ProjectPaths.PROJECT, 'node_modules'))
 
     @staticmethod
     def poetry_install() -> None:
@@ -35,10 +35,10 @@ class CleanupController(ControllerBase):
     @staticmethod
     def delete_venv() -> None:
         """Deletes the virtual environment folder and assets."""
-        shutil.rmtree(os.path.join(ProjectDirPaths.ROOT, 'env'))
+        shutil.rmtree(os.path.join(ProjectPaths.ROOT, 'env'))
 
     @staticmethod
     def remove_files() -> None:
         """Removes redundant files."""
-        os.remove(os.path.join(ProjectDirPaths.PROJECT, '__init__.py'))
+        os.remove(os.path.join(ProjectPaths.PROJECT, '__init__.py'))
 
