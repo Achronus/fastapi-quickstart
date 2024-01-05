5bfefaefbee583cc7e0b2003ae061df7be22ced9
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-01-05 20:46:17 +0000

    added test structure

--- fastapi_quickstart/tests/test_filepaths.py
+++ fastapi_quickstart/tests/test_filepaths.py
@@ -1,2 +1,56 @@
 import pytest
 
+from conf.constants.filepaths import ProjectPaths, SetupDirPaths
+
+
+def test_project_name_valid() -> None:
+    pass
+
+
+class TestProjectPaths:
+    def test_root_valid() -> None:
+        pass
+
+    def test_project_valid() -> None:
+        pass
+
+    def test_init_poetry_conf_valid() -> None:
+        pass
+
+    def test_init_readme_valid() -> None:
+        pass
+
+    def test_poetry_conf_valid() -> None:
+        pass
+
+    def test_project_main_valid() -> None:
+        pass
+
+    def test_project_build_valid() -> None:
+        pass
+
+    def test_static_valid() -> None:
+        pass
+
+    def test_static_css_valid() -> None:
+        pass
+
+    def test_static_js_valid() -> None:
+        pass
+
+    def test_static_imgs_valid() -> None:
+        pass
+
+
+class TestSetupDirPaths:
+    def test_root_valid() -> None:
+        pass
+
+    def test_setup_root_valid() -> None:
+        pass
+
+    def test_assets_valid() -> None:
+        pass
+
+    def test_project_name_valid() -> None:
+        pass
