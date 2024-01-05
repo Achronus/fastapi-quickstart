20c66464759a7adcf6338178366b7aded9184577
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-01-05 20:58:12 +0000

    added dir split helper

--- fastapi_quickstart/tests/test_filepaths.py
+++ fastapi_quickstart/tests/test_filepaths.py
@@ -1,6 +1,21 @@
+import os
 import pytest
 
 from conf.constants.filepaths import ProjectPaths, SetupDirPaths
+
+
+def __directory_splitter(dir_path: str) -> list[str]:
+    """Helper function for splitting a path into a list of directories."""
+    _, path_to_split = os.path.splitdrive(dir_path)
+    directories = []
+
+    while path_to_split:
+        path_to_split, directory = os.path.split(path_to_split)
+        
+        if directory:
+            directories.insert(0, directory)
+
+    return directories
 
 
 def test_project_name_valid() -> None:
