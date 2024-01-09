2f8f773b8b6ee7ece9619435f96e1d79b88b528c
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-01-09 06:33:29 +0000

    added tests

--- fastapi_quickstart/tests/test_filepaths.py
+++ fastapi_quickstart/tests/test_filepaths.py
@@ -1,71 +1,141 @@
 import os
 import pytest
 
-from conf.constants.filepaths import ProjectPaths, SetupDirPaths
+from conf.constants.filepaths import (
+    ProjectPaths, 
+    SetupDirPaths, 
+    get_project_name,
+    set_project_name,
+    AssetFilenames,
+    SetupAssetsDirNames
+)
+from conf.constants import STATIC_DIR_NAME
+
+PROJECT_NAME = 'test_project'
+
+set_project_name(PROJECT_NAME)
+CUSTOM_PROJECT_PARENT = get_project_name()
+CUSTOM_PROJECT_ROOT = os.path.join(CUSTOM_PROJECT_PARENT, get_project_name())
+CUSTOM_PROJECT_STATIC = os.path.join(CUSTOM_PROJECT_ROOT, STATIC_DIR_NAME)
+
+SETUP_ROOT = 'fastapi-quickstart'
+SETUP_PROJECT_ROOT = os.path.join(SETUP_ROOT, 'fastapi_quickstart')
+STATIC = os.path.join(SETUP_PROJECT_ROOT, STATIC_DIR_NAME)
 
 
-def __directory_splitter(dir_path: str) -> list[str]:
-    """Helper function for splitting a path into a list of directories."""
-    _, path_to_split = os.path.splitdrive(dir_path)
-    directories = []
-
-    while path_to_split:
-        path_to_split, directory = os.path.split(path_to_split)
-        
-        if directory:
-            directories.insert(0, directory)
-
-    return directories
+@pytest.fixture
+def project_paths() -> None:
+    set_project_name(PROJECT_NAME)
+    return ProjectPaths()
 
 
-def test_project_name_valid() -> None:
-    pass
+def test_project_name_valid(project_paths: ProjectPaths) -> None:
+    set_project_name(ProjectPaths().PROJECT_NAME)
+    assert get_project_name() is not None
+    assert get_project_name() != ''
+    assert get_project_name != project_paths.PROJECT_NAME
 
 
 class TestProjectPaths:
-    def test_root_valid() -> None:
-        pass
+    @pytest.fixture(autouse=True)
+    def initialize_project_paths(self) -> None:
+        set_project_name(PROJECT_NAME)
+        self.project_paths = ProjectPaths()
 
-    def test_project_valid() -> None:
-        pass
+    def __validate_path(self, directory_path: str, ending: str) -> None:
+        assert directory_path.endswith(ending)
 
-    def test_init_poetry_conf_valid() -> None:
-        pass
+    def test_root_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.ROOT, 
+            CUSTOM_PROJECT_PARENT
+        )
 
-    def test_init_readme_valid() -> None:
-        pass
+    def test_project_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.PROJECT, 
+            CUSTOM_PROJECT_ROOT
+        )
 
-    def test_poetry_conf_valid() -> None:
-        pass
+    def test_init_poetry_conf_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.INIT_POETRY_CONF, 
+            os.path.join(CUSTOM_PROJECT_ROOT, AssetFilenames.POETRY_CONF)
+        )
 
-    def test_project_main_valid() -> None:
-        pass
+    def test_init_readme_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.INIT_README, 
+            os.path.join(CUSTOM_PROJECT_ROOT, AssetFilenames.README)
+        )
 
-    def test_project_build_valid() -> None:
-        pass
+    def test_poetry_conf_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.POETRY_CONF, 
+            os.path.join(CUSTOM_PROJECT_PARENT, AssetFilenames.POETRY_CONF)
+        )
 
-    def test_static_valid() -> None:
-        pass
+    def test_project_main_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.PROJECT_MAIN, 
+            os.path.join(CUSTOM_PROJECT_ROOT, AssetFilenames.MAIN)
+        )
 
-    def test_static_css_valid() -> None:
-        pass
+    def test_project_build_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.PROJECT_BUILD, 
+            os.path.join(CUSTOM_PROJECT_ROOT, AssetFilenames.BUILD)
+        )
 
-    def test_static_js_valid() -> None:
-        pass
+    def test_static_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.STATIC,
+            CUSTOM_PROJECT_STATIC 
+        )
 
-    def test_static_imgs_valid() -> None:
-        pass
+    def test_static_css_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.CSS, 
+            os.path.join(CUSTOM_PROJECT_STATIC, SetupAssetsDirNames.CSS)
+        )
+
+    def test_static_js_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.JS, 
+            os.path.join(CUSTOM_PROJECT_STATIC, SetupAssetsDirNames.JS)
+        )
+
+    def test_static_imgs_valid(self) -> None:
+        self.__validate_path(
+            self.project_paths.IMGS, 
+            os.path.join(CUSTOM_PROJECT_STATIC, SetupAssetsDirNames.IMGS)
+        )
 
 
 class TestSetupDirPaths:
-    def test_root_valid() -> None:
-        pass
+    def __validate_path(self, directory_path: str, ending: str) -> None:
+        assert directory_path.endswith(ending)
 
-    def test_setup_root_valid() -> None:
-        pass
+    def test_root_valid(self) -> None:
+        self.__validate_path(
+            SetupDirPaths.ROOT, 
+            SETUP_ROOT
+        )
 
-    def test_assets_valid() -> None:
-        pass
+    def test_setup_root_valid(self) -> None:
+        self.__validate_path(
+            SetupDirPaths.SETUP_ROOT, 
+            SETUP_PROJECT_ROOT
+        )
 
-    def test_project_name_valid() -> None:
-        pass
+    def test_assets_valid(self) -> None:
+        self.__validate_path(
+            SetupDirPaths.ASSETS, 
+            os.path.join(SETUP_PROJECT_ROOT, SetupAssetsDirNames.ROOT)
+        )
+
+    def test_project_name_valid(self) -> None:
+        self.__validate_path(
+            SetupDirPaths.PROJECT_NAME, 
+            os.path.join(SETUP_PROJECT_ROOT, 'conf', 'name')
+        )
